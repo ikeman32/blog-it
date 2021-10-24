f6fd33a63544c9ce66a4e1a0cf59a711bef27672
ikeman32 <david.isakson.ii@gmail.com>
2021-10-23 23:53:52 -0700

    creat login card

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,11 +1,22 @@
 import React from "react"
 import LoginCard from "./logincard"
 
-const Login = ()=>{
-    return(
-        <LoginCard>
-            <p>login</p>
-        </LoginCard>
+// Language: javascript
+// Create login component
+
+const Login = () => {
+    return (
+        <div className="login">
+            <LoginCard>
+                <h1>Login</h1>
+                <form>  
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button>Login</button> 
+                </form>
+            </LoginCard>
+
+        </div>  
     )
 }
 
